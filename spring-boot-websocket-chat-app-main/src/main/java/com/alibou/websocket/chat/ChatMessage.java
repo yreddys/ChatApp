926a314e9b45e756b1241c40e5670eab9f97f2ac
maddi <maddi@DESKTOP-SIS50D8.mshome.net>
2023-06-13 10:19:23 +0530

    Commit

---
+++ spring-boot-websocket-chat-app-main/src/main/java/com/alibou/websocket/chat/ChatMessage.java
@@ -0,0 +1,50 @@
+package com.alibou.websocket.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+	public MessageType getType() {
+		return type;
+	}
+	public void setType(MessageType type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public ChatMessage(MessageType type, String content, String sender) {
+		super();
+		this.type = type;
+		this.content = content;
+		this.sender = sender;
+	}
+	
+	ChatMessage(){}
+	public static Object builder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	}
+    
+	
+
+
